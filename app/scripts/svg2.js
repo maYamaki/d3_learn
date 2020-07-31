9acd291b881749bdde7fd63efc3a57d1d199ae5e
ma <ma_yamaki@yahoo.com>
2020-08-01 08:27:31 +0900

    fin svg2

--- app/scripts/svg2.js
+++ app/scripts/svg2.js
@@ -6,19 +6,24 @@ var dataArray = [4,15,34];
 
 //add an svg element
 
-const svg = canvas.append('svg')
-            .attr('width',500)
-            .attr("height", 250);
+// const svg = canvas.append('svg')
+//             .attr('width',500)
+//             .attr("height", 250);
 
+const svg = canvas.select('svg');
 
-const rect = svg.append("rect");
+// const rect = svg.append("rect");
+const rect = svg.selectAll('rect');
 
 rect.attr("width", 24)
     .data(dataArray)
     .attr("fill", "orange")
-    .attr("height", 100)
-    .attr('y', function (d, i) {
-                        console.log(i);
-                        return  d*12;
-            })
-    .attr("x", function(d, i) {console.log(d) return d*90;});
+    .attr("height", function(d){
+      return d*2;
+    })
+    // .attr('y', function (d, i) {
+    //                     return  d;
+    //         })
+    .attr("x", function(d, i) {
+                                return i*25;});
+console.log(rect);
