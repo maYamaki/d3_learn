772785efc04a661be06342521f1c1ba810493703
ma <ma_yamaki@yahoo.com>
2020-08-01 17:35:10 +0900

    fin

--- app/scripts/enter.js
+++ app/scripts/enter.js
@@ -1,10 +1,14 @@
 const canvas = d3.select(".canva");
+/*
+JSON - Javascript Object Nortation
+*/
+
 
 var dataArray = [
-  {width: 25, height: 4, fill: "pink"},
-  {width: 25, height: 14, fill: "purple"},
-  {width: 25, height: 44, fill: "red"},
-  {width: 25, height: 34, fill: "blue"}
+  {"width": 25, "height": 4, "fill": "pink"},
+  {"width": 25, "height": 14, "fill": "purple"},
+  {"width": 25, "height": 44, "fill": "red"},
+  {"width": 25, "height": 34, "fill": "blue"}
 ]
 
 //add an svg element
@@ -17,8 +21,8 @@ const rect = svg.selectAll('rect');
 rect.data(dataArray)
     .enter().append("rect")
     .attr("width",24)
-    .attr("fill", function (d) {return d.fill;})
-    .attr("height",function (d){ return d.height*2;})
+    .attr("fill",  (d, i) => d.fill)
+    .attr("height",d => d.height*2)
     .attr("x", function(d, i){
       console.log(d);
        return i*25;})
